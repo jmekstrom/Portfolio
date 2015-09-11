5471725715f39fc01446bdcbdcc661d4e678abb2
JEkstroms <jmekstrom@gmail.com>
2015-09-11 11:09:43 -0700

    php mailer test

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -1,31 +1,75 @@
 <?php
+//	// Contact
+//	$to = 'jmekstrom@gmail.com';
+//    $subject = 'Job Opportunity';
+//
+//	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_message'])){
+//   		$name    = $_POST['c_name'];
+//		$headers = "From: " . strip_tags($_POST['c_email']) . "\r\n";
+//		$headers .= "Reply-To: " . strip_tags($_POST['c_email']) . "\r\n";
+//		$headers .= "CC: jxstrom@example.com\r\n";
+//		$headers .= "MIME-Version: 1.0\r\n";
+//		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+//    	$message = $_POST['c_message'];
+//
+//		if (mail($to, $subject, $message, $headers)) {
+//			$result = array(
+//				'message' => 'Thanks for contacting me!',
+//				'sendstatus' => 1
+//				);
+//			echo json_encode($result);
+//		} else {
+//			$result = array(
+//				'message' => 'Sorry, something is wrong',
+//				'sendstatus' => 1
+//				);
+//			echo json_encode($result);
+//		}
+//	}
+//
+//?>
 
-	// Contact
-	$to = 'jmekstrom@gmail.com';
-    $subject = 'Job Opportunity';
+<?php
+require_once('php/class.phpmailer.php');
+//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
 
-	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_message'])){
-   		$name    = $_POST['c_name'];
-		$headers = "From: " . strip_tags($_POST['c_name']) . "\r\n";
-		$headers .= "Reply-To: " . strip_tags($_POST['c_email']) . "\r\n";
-		$headers .= "CC: jxstrom@example.com\r\n";
-		$headers .= "MIME-Version: 1.0\r\n";
-		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-    	$message = $_POST['c_message'];
+$mail = new PHPMailer();
 
-		if (mail($to, $subject, $message, $headers)) {
-			$result = array(
-				'message' => 'Thanks for contacting me!',
-				'sendstatus' => 1
-				);
-			echo json_encode($result);
-		} else { 
-			$result = array(
-				'message' => 'Sorry, something is wrong',
-				'sendstatus' => 1
-				);
-			echo json_encode($result);
-		} 
-	}
+$body = file_get_contents('contents.html');
+$body = eregi_replace("[\]", '', $body);
+
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host = "mail.yourdomain.com"; // SMTP server
+$mail->SMTPDebug = 2;                     // enables SMTP debug information (for testing)
+// 1 = errors and messages
+// 2 = messages only
+$mail->SMTPAuth = true;                  // enable SMTP authentication
+$mail->SMTPSecure = "tls";                 // sets the prefix to the servier
+$mail->Host = "smtp.gmail.com";      // sets GMAIL as the SMTP server
+$mail->Port = 587;                   // set the SMTP port for the GMAIL server
+$mail->Username = "yourusername@gmail.com";  // GMAIL username
+$mail->Password = "yourpassword";            // GMAIL password
+
+$mail->SetFrom('name@yourdomain.com', 'First Last');
+
+$mail->AddReplyTo("name@yourdomain.com", "First Last");
+
+$mail->Subject = "PHPMailer Test Subject via smtp (Gmail), basic";
+
+$mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+
+$mail->MsgHTML($body);
+
+$address = "whoto@otherdomain.com";
+$mail->AddAddress($address, "John Doe");
+
+$mail->AddAttachment("images/phpmailer.gif");      // attachment
+$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+
+if (!$mail->Send()) {
+	echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+	echo "Message sent!";
+}
 
 ?>
